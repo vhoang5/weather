060b64e070b69e7ef981b8c26992c52919a38dde
An Hoang Van <an.hoang>
2024-05-08 10:01:56 +0700

    remove navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <!-- <v-app-bar color="surface-variant" title="Weather App"></v-app-bar> -->
-
-    <v-navigation-drawer>
-    </v-navigation-drawer>
-
-    <v-navigation-drawer location="right">
-
-    </v-navigation-drawer>
-
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
       <router-view></router-view>
     </v-main>
